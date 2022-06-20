7284da08a0b14090497d634c547b78fcc390023d
tunguyen1993 <s2.shining93@gmail.com>
2022-06-20 12:08:56 +0700

    update

--- src/core/middleware/frontend.middleware.ts
+++ src/core/middleware/frontend.middleware.ts
@@ -23,13 +23,18 @@ export class FrontendMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     const { url } = req;
     if (url.indexOf(ROUTE_PREFIX) === 1) {
+      console.log(444444444);
+
       // it starts with /api --> continue with execution
       next();
     } else if (allowedExt.filter((ext) => url.indexOf(ext) > 0).length > 0) {
+      console.log(2222222222);
+
       // it has a file extension --> resolve the file
       console.log("here be the other files: " + resolvePath(url));
       res.sendFile(resolvePath(url));
     } else {
+      console.log(1111111111);
       // in all other cases, redirect to the index.html!
       res.sendFile(resolvePath("index.html"));
     }
