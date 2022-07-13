8c56a5d4b5cc331cafcc827451fce73e6365ac1e
tunguyen1993 <s2.shining93@gmail.com>
2022-07-13 10:06:26 +0700

    build config

--- src/modules/config/admin.config.controller.ts
+++ src/modules/config/admin.config.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Get, Param, Put } from "@nestjs/common";
+import {
+  Body,
+  CACHE_MANAGER,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Put,
+} from "@nestjs/common";
 import { ConfigService } from "./config.service";
 
 @Controller("api/v1/admin/config")
 export class AdminConfigController {
-  constructor(private _configService: ConfigService) {}
+  constructor(
+    private _configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager,
+  ) {}
 
   @Get()
   async data() {
@@ -14,11 +25,31 @@ export class AdminConfigController {
     };
   }
 
+  @Put("fake-data")
+  async update() {
+    let fake_data = await this.cacheManager.get("FAKE_DATA");
+    await this.cacheManager.set("FAKE_DATA", !fake_data, { ttl: 0 });
+    return {
+      code: 200,
+      data: await this.cacheManager.get("FAKE_DATA"),
+      error: null,
+    };
+  }
+
   @Put(":id")
   async updateData(@Param("id") id: number, @Body() body) {
     return {
       code: 200,
       data: await this._configService.updateConfig(id, body),
+      error: null,
+    };
+  }
+
+  @Get("fake-data")
+  async fakeData() {
+    return {
+      code: 200,
+      data: await this.cacheManager.get("FAKE_DATA"),
       error: null,
     };
   }
